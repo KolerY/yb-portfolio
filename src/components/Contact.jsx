7a3d501ebc20869ea7688797ce1acaca31c983c3
Yannick Blanchette <yannickblanchette@hotmail.com>
2024-11-13 02:29:13 -0500

    Ajout form contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,11 +4,22 @@ import { CONTACT } from '../constants'
 const Contact = () => {
   return (
     <div className='border-b border-neutral-900 pb-20'>
-        <h2 className='my-10 text-center text-4xl'>Prendre Contact</h2>
-        <div className='text-center tracking-tighter'>
-            <p className='my-4'>{CONTACT.address}</p>
-            <a href="mailto:yannickblanchette1337@gmail.com">{CONTACT.email}</a>
+      <section class="dark:bg-gray-900">
+        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-white dark:text-white">Contact</h2>
+          <form action="#" class="space-y-8">
+            <div>
+              <label for="email" class="block mb-2 text-sm font-medium text-white dark:text-gray-300">Ton email</label>
+              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="email@xyz.com" required></input>
+            </div>
+            <div class="sm:col-span-2">
+              <label for="message" class="block mb-2 text-sm font-medium text-white dark:text-gray-400">Ton message</label>
+              <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Message..."></textarea>
+            </div>
+            <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-gray-100 rounded-lg bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-500 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600 transition duration-200 ease-in-out">Envoyer</button>
+          </form>
         </div>
+      </section>
     </div>
   )
 }
