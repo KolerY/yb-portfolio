369a6da24ac1f5b0b74a80bb62e4233c2b75de69
Yannick Blanchette <yannickblanchette@hotmail.com>
2024-12-01 02:37:48 -0500

    Ajout footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer class="rounded-lg shadow m-4 dark:bg-gray-800">
+            <div class="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
+                <span class="text-sm text-purple-500 sm:text-center dark:text-gray-400">
+                    © 2024 <a href="https://flowbite.com/" class="hover:underline">Yannick Blanchette</a>. Tout droits réservés.
+                </span>
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer
