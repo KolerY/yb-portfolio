b9372d163af7d1aa70ceaf89adb4fe0d80adac2c
Yannick Blanchette <yannickblanchette@hotmail.com>
2024-12-02 11:15:10 -0500

    Ajout close button / responsive mobile modal

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import { HERO_CONTENT } from '../constants/index.js'
 import profilePic from '../assets/profile-pic-transparent.png'
 import { motion } from 'framer-motion'
 import resume from '../assets/resume.png'
+import { AiOutlineClose, AiOutlineDownload } from "react-icons/ai";
 
 const container = (delay) => ({
     hidden: { x: -100, opacity: 0 },
@@ -16,20 +17,42 @@ const container = (delay) => ({
 const Modal = ({ isOpen, onClose, resume }) => {
     if (!isOpen) return null;
 
+    const downloadResume = () => {
+        const link = document.createElement("a");
+        link.href = resume;
+        link.download = "Resume.png";
+        link.click();
+    };
+
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded-lg shadow-lg w-11/12 max-w-4xl">
-                <h2 className="text-xl font-bold mb-4">Curriculum Vitae</h2>
+            <div className="bg-white p-4 rounded-lg shadow-lg w-11/12 max-w-4xl relative">
+                {/* Close Button */}
+                <button
+                    onClick={onClose}
+                    className="absolute top-2 right-2 bg-purple-600 text-white p-2 rounded-full hover:bg-purple-700 focus:ring-2 focus:ring-purple-600 focus:outline-none"
+                    aria-label="Close Modal"
+                >
+                    <AiOutlineClose size={20} />
+                </button>
+
+                {/* Title */}
+                <h2 className="text-xl font-bold mb-4 text-center">Curriculum Vitae</h2>
+
+                {/* Resume Image */}
                 <img
                     src={resume}
                     alt="Resume"
-                    className="mb-4 w-full h-auto max-h-[800px] object-contain"
+                    className="mb-4 w-full h-auto max-h-[70vh] object-contain"
                 />
+
+                {/* Download Button */}
                 <button
-                    onClick={onClose}
-                    className="bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700"
+                    onClick={downloadResume}
+                    className="flex items-center gap-2 bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 mx-auto"
                 >
-                    Close
+                    <AiOutlineDownload size={20} />
+                    <span>Télécharger</span>
                 </button>
             </div>
         </div>
